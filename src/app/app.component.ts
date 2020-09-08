c77004310e1bcc5f148d9b353501e472a7f4921e
drewberry27 <drewberrysteph@gmail.com>
2020-09-08 12:06:15 +0800

    v2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,30 +2,8 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: "app-root",
-  template: `
-    <div>
-      <h1>Congratulations!</h1>
-      <h4>You got your component to display!</h4>
-      <hr />
-      <h5>How does your event look below?</h5>
-
-      <div style="margin-top:30px">Event: {{ event.name }}</div>
-      <div>Date:{{ event.data }}</div>
-      <div>Time:{{ event.time }}</div>
-      <div>Address:{{ event.address }}</div>
-    </div>
-  `,
+  template: "<event-app> </event-app>",
 })
 export class AppComponent {
   title = "ng-exercise";
-  event = {
-    name: "ngConf 2025",
-    date: "3/1/2025",
-    time: "8am",
-    location: {
-      address: "123 Main St",
-      city: "Salt Lake City, UT",
-      country: "USA",
-    },
-  };
 }
