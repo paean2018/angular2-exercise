1baad3c1d70748be32e137002048c243682ffc47
drewberry27 <drewberrysteph@gmail.com>
2020-09-08 15:30:07 +0800

    done

--- src/app/child.component.ts
+++ src/app/child.component.ts
@@ -3,24 +3,27 @@ import { Component, Output, EventEmitter } from "@angular/core";
 @Component({
   selector: "child",
   template: `
-    <div>
-      <h1>Child Component: {{ counter }}</h1>
-      <button class="btn btn-primary" (click)="handleClick()">Click me</button>
+    <div *ngIf="showMsg">
+      <h1>Congratulations</h1>
+      <h2>You stopped the child component's counter!</h2>
     </div>
+    <h1>Child Component: {{ counter }}</h1>
   `,
 })
 export class ChildComponent {
   @Output() eventClick = new EventEmitter();
-
+  private running: any;
+  private showMsg = false;
   counter: number = 0;
 
   ngOnInit() {
-    setInterval(() => {
+    this.running = setInterval(() => {
       this.counter++;
     }, 1000);
   }
 
-  handleClick() {
-    this.eventClick.emit(this.counter);
+  stopTimer() {
+    clearInterval(this.running);
+    this.showMsg = true;
   }
 }
--- src/app/parent.component.ts
+++ src/app/parent.component.ts
@@ -2,11 +2,16 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: "parent",
-  template: ` <div *ngIf="currentCounter">
+  template: `
+    <div *ngIf="currentCounter">
       <h1>Congratulations!</h1>
       <h3>You set the parent's counter to: {{ currentCounter }}</h3>
     </div>
-    <child (eventClick)="handleClick($event)"> </child>`,
+    <child #child> </child>
+    <button class="btn btn-primary" (click)="child.stopTimer()">
+      Stop Timer
+    </button>
+  `,
 })
 export class ParentComponent {
   currentCounter: number;
