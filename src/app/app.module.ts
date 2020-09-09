2b5a8a159413e89cd1ea1b4ffae3bb11fc2f849c
drewberry27 <drewberrysteph@gmail.com>
2020-09-09 12:07:06 +0800

    done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,12 @@ import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
 import { EventsListComponent } from "./events-list.component";
+import { EventService } from "./event.service";
 
 @NgModule({
   declarations: [AppComponent, EventsListComponent],
   imports: [BrowserModule],
-  providers: [],
+  providers: [EventService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/events-list.component.ts
+++ src/app/events-list.component.ts
@@ -1,66 +1,24 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { EventService } from "./event.service";
 
 @Component({
   selector: "event-details",
   templateUrl: "./events-list.component.html",
   styles: [
     `
-      .legend span {
-        padding-right: 30px;
-      }
-      .thumbnail {
-        margin-top: 10px;
-        padding-top: 5px;
-      }
-      .label {
-        font-size: 14px;
-        margin-bottom: 10px;
-        display: inline-block;
+      th,
+      td {
+        font-size: 16px;
+        padding: 5px 10px;
       }
     `,
   ],
 })
-export class EventsListComponent {
-  events = [
-    {
-      name: "Angular Connect",
-      date: "9/26/2036",
-      time: "10am",
-      location: {
-        address: "1 London Road ",
-        city: "London",
-        country: "England",
-      },
-      format: "InPerson",
-    },
-    {
-      name: "ng-conf 2037",
-      date: "4/15/2037",
-      time: "9am",
-      onlineUrl: "https://www.ng-conf.org/",
-      format: "Online",
-    },
-    { name: "Future Conf (Location/Url TBD)", date: "6/10/2037", time: "8am" },
-    {
-      name: "UN Angular Summit",
-      date: "6/10/2037",
-      time: "8am",
-      location: {
-        address: "The UN Angular Center",
-        city: "New York",
-        country: "USA",
-      },
-      format: "InPerson",
-    },
-  ];
+export class EventsListComponent implements OnInit {
+  events: any[];
+  constructor(private eventService: EventService) {}
 
-  addStyle(format) {
-    if (format === "Online") {
-      return { color: "green" };
-    } else if (format === "InPerson") {
-      return { color: "red" };
-    } else {
-      return {};
-    }
+  ngOnInit() {
+    this.events = this.eventService.getEvents();
   }
 }
